d99f71d71ff18c8194617f79f2f848fe3a2975e0
Adam McCrea <adam@adamlogic.com>
2012-11-13 17:16:13 -0500

    Don't assume we're in the context of a rake command

--- lib/middleman-gh-pages.rb
+++ lib/middleman-gh-pages.rb
@@ -1,7 +1,5 @@
 require "middleman-gh-pages/version"
 
-module Middleman
-  module GithubPages
-    Rake.add_rakelib(File.expand_path('../middleman-gh-pages/tasks', __FILE__))
-  end
+if defined?(Rake)
+  Rake.add_rakelib(File.expand_path('../middleman-gh-pages/tasks', __FILE__))
 end
